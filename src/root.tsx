2b32e8d0c4fee9d359cb82034c6af498bc92a7bd
Daniel Caldera <bdcaldera@gmail.com>
2023-03-23 18:43:24 -0600

    Added Vercel Edge

--- src/root.tsx
+++ src/root.tsx
@@ -1,8 +1,9 @@
-import { component$ } from '@builder.io/qwik';
-import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
-import { RouterHead } from './components/router-head/router-head';
+import { component$ } from '@builder.io/qwik'
+import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city'
+import { RouterHead } from './components/router-head/router-head'
 
-import './global.css';
+// import './global.css';
+import 'tailwindcss/tailwind.css'
 
 export default component$(() => {
   /**
@@ -15,14 +16,14 @@ export default component$(() => {
   return (
     <QwikCityProvider>
       <head>
-        <meta charSet="utf-8" />
-        <link rel="manifest" href="/manifest.json" />
+        <meta charSet='utf-8' />
+        <link rel='manifest' href='/manifest.json' />
         <RouterHead />
       </head>
-      <body lang="en">
+      <body lang='en'>
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>
     </QwikCityProvider>
-  );
-});
+  )
+})
