9c0c1c26c5dea41feec3013aa751a46851768c1b
Daniel Caldera <bdcaldera@gmail.com>
2023-03-23 19:31:10 -0600

    asd@

--- src/routes/chat/layout.tsx
+++ src/routes/chat/layout.tsx
@@ -1,6 +1,9 @@
 import { component$, Slot } from '@builder.io/qwik'
 import { routeLoader$ } from '@builder.io/qwik-city'
 
+import Header from '~/components/starter/header/header'
+import Footer from '~/components/starter/footer/footer'
+
 export const useServerTimeLoader = routeLoader$(() => {
   return {
     date: new Date().toISOString()
@@ -11,12 +14,12 @@ export default component$(() => {
   return (
     <div class='page'>
       <main>
-        <>header</>
+        <Header />
         <Slot />
       </main>
       <div class='section dark'>
         <div class='container'>
-          <>footer</>
+          <Footer />
         </div>
       </div>
     </div>
