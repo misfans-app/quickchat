dd3d4043ece3011445a45510e3d47bf55d0314c7
Tomas Duclos <tomasmarcelo.duclos@gmail.com>
2023-03-24 15:44:29 -0300

    ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,32 @@
-import { component$ } from '@builder.io/qwik'
+import { component$, $ } from '@builder.io/qwik'
+import { Form } from '@builder.io/qwik-city'
 import Input from '~/components/chat/Input'
 import ChatBox from '~/components/chat/ChatBox'
 
+
 export default component$(() => {
+  const handleOnSubmit = $((e: any) => {
+    e.preventDefault()
+
+    const { message } = Object.fromEntries(new FormData(e.currentTarget))
+
+    if (!message) return
+
+    console.log(message)
+
+  })
+
   return (
-    <div>
-      <Input />
-      <ChatBox />
+    <div class="h-screen flex items-end pb-8">
+      <div class="w-full flex flex-col gap-8">
+        <div class="max-h-[calc(100vh-8rem)] overflow-y-scroll">
+          <ChatBox/>
+        </div>
+        
+        <Form preventdefault:submit onSubmit$={handleOnSubmit}>
+          <Input/>
+        </Form>
+      </div>
     </div>
   )
 })
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,7 +2,7 @@ import { component$, Slot } from '@builder.io/qwik'
 
 export default component$(() => {
   return (
-    <main class="max-w-screen-lg mx-auto py-4">
+    <main class="max-w-screen-lg mx-auto">
       <Slot/>
     </main>
   )
